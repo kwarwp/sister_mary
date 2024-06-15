448ca2efe51673174067fcd3f08e87a0352f542f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-15 14:19:06 -0300

    Created stacy/__log__.py @2024-06-15 17:19

---
+++ stacy/__log__.py
@@ -0,0 +1,7 @@
+
+{'date': 'Sat Jun 15 2024 14:19:04.741 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: expected expression, got end of script
+  module '$exec_388' line 9
+game <= html.IMG(onmousclick="https://i.imgur.com/kJrBXt8.jpeg", src="https://i.imgur.com/4lZtBuP.jpeg")
+'''},
