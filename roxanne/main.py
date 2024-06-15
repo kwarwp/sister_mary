2758707e7982df18b9ba090bb7a1c4d954d255fa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-15 13:29:01 -0300

    Saved roxanne/main.py @2024-06-15 16:29

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,17 +1,25 @@
 # sister_mary.roxanne.main.py
 # Jogo do Gato
 from browser import document, html
-class Recusos:
+class Recursos:
     CENA0 = "https://i.imgur.com/kJrBXt8.jpeg"
     HEROI0 = "https://i.imgur.com/WQ2l5kM.png"
     
     
 class Cena:
     def __init__(self, recurso_cena, jogo):
-        self.cena = html.DIV(Id="_cena")
+        self.cena = html.DIV(Id="_cena_")
         self.cena <= html.IMG(src=recurso_cena)
         jogo <= self.cena
+    
+    
+class Heroi:
+    def __init__(self, recurso_heroi, jogo):
+        self.heroi = html.DIV(Id="_heroi_", style=dict(position="absolute", top="400px"))
+        self.heroi <= html.IMG(src=recurso_heroi)
+        jogo <= self.heroi
         
         
 if __name__ == "__main__":
     Cena(Recursos.CENA0, document["pydiv"])
+    Heroi(Recursos.HEROI0, document["pydiv"])
