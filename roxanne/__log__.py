aa85639473524fbe7aeced9dbaf3d851dc0f29bf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-15 13:37:33 -0300

    Saved roxanne/__log__.py @2024-06-15 16:37

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -15,3 +15,25 @@
     Cena(Recursos.CENA0, document["pydiv"])
 NameError: name 'Recursos' is not defined
 '''},
+{'date': 'Sat Jun 15 2024 13:37:32.979 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    Jogo()
+  module <module> line 34
+    Heroi(Recursos.HEROI0, self.jogo)        
+  module <module> line 18
+    self.heroi = html.DIV(Id="_heroi_", style=dict(
+  module <module> line 1
+    (self.x)
+AttributeError: 'Heroi' object has no attribute 'x'
+'''},
