0009fa612795c09ce0f7f565ec10233ebc730b14
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-15 13:37:29 -0300

    Saved roxanne/main.py @2024-06-15 16:37

--- roxanne/main.py
+++ roxanne/main.py
@@ -18,7 +18,7 @@ class Heroi:
         self.heroi = html.DIV(Id="_heroi_", style=dict(
         position="absolute", left=f"{self.x}px", top="400px", transform="scaleX(-1)"))
         self.heroi <= html.IMG(src=recurso_heroi)
-        self.hero.bind("click", self.anda)
+        self.heroi.bind("click", self.anda)
         jogo <= self.heroi
         
     def anda(self, ev=0):
