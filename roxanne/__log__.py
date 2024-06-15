9c411cff788bc3c95d47d6f34bd716a4dc44e26e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-06-15 16:30:37 -0300

    Saved roxanne/__log__.py @2024-06-15 19:30

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -44,3 +44,23 @@ AttributeError: 'Heroi' object has no attribute 'x'
                                                          ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Sat Jun 15 2024 16:30:37.91 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    Jogo()
+  module <module> line 47
+    self.cena = Cena(Recursos.CENA1, self)
+  module <module> line 14
+    self.cena.style.update({"background-image": recurso_cena})
+AttributeError: 'CSSProperty' object has no attribute 'update'
+'''},
