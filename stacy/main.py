1100d863a9d64dc956be5535ece1647bc2e27f4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-07-15 09:22:53 -0300

    Saved stacy/main.py @2024-07-15 12:22

--- stacy/main.py
+++ stacy/main.py
@@ -1,2 +1,22 @@
 # sister_mary.stacy.main.py
 # Calculadora
+
+class Calculadora:
+    def __init__(self):
+        self.ops = dict(a=None, d=None, m=None)
+        self.reg = 0
+        
+    def add(self, op):
+        result = self.reg + op
+        
+    def operate(self):
+        self.reg = input("escolha um número:")
+        get_ops = input("escolha uma operção (a, d, m):")
+        op = input("escolha outro número:")
+        if get_ops in self.ops:
+            self.ops[get_ops](op)
+        else:
+            input("operação inválida")
+        
+Calculadora().add().operate()
+        
