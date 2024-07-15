9ffa2048946c5eaf684c82311eae6d1ca4228c18
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-07-15 09:40:24 -0300

    Saved stacy/__log__.py @2024-07-15 12:40

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -71,3 +71,28 @@ IndentationError: expected an indented block
                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Jul 15 2024 09:40:23.638 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Insiraos dois valores:
+5 * 5 = 25
+Insiraos dois valores:
+5 * 5 = 25
+Insiraos dois valores:
+5 * 5 = 25
+Insiraos dois valores:
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    Calculadora().run()
+  module <module> line 31
+    val1 = int(input('valor 1;'))
+ValueError: invalid literal for int() with base 10: ''
+'''},
