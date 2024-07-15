28f104fe8a6ffd7a01fa45e453416cf83acf8c64
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-07-15 09:23:47 -0300

    Saved stacy/__log__.py @2024-07-15 12:23

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -27,3 +27,19 @@ game <= html.IMG(onmousclick="https://i.imgur.com/kJrBXt8.jpeg", src="https://i.
     Calculadora().add().operate()
 TypeError: add() missing 1 positional argument: op
 '''},
+{'date': 'Mon Jul 15 2024 09:23:46.126 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 21
+    Calculadora().add().operate()
+TypeError: add() missing 1 positional argument: op
+'''},
