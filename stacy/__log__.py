17941f0d9617cf1d83f8eadc80df2c710a808a9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-07-16 19:59:52 -0300

    Saved stacy/__log__.py @2024-07-16 22:59

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -153,3 +153,23 @@ TypeError: add() missing 1 positional argument: op
     self.ops[self.options](op)
 TypeError: 'NoneType' object is not callable
 '''},
+{'date': 'Tue Jul 16 2024 19:59:51.662 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 62
+    Calculadora().run()
+  module <module> line 38
+    while self.operate():
+  module <module> line 27
+    self.reg = int(input("escolha um número:"))
+ValueError: invalid literal for int() with base 10: 'ok'
+'''},
