f8862a140a1f758dcc39f27246ffaedba03a8a9c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-18 13:10:23 -0300

    Saved libby/main.py @2024-09-18 16:10

--- libby/main.py
+++ libby/main.py
@@ -1,2 +1,9 @@
 # sister_mary.libby.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto 
+
+explorandoPorao = "view-source:https://designer.microsoft.com/image-creator"
+
+def Historia1 ():
+	primeiraCena = Cena(img = "view-source:https://designer.microsoft.com/image-creator")
+	primeiraCnea.vai()
+Historia1()
