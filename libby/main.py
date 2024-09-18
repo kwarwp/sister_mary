327274a4129a2e59eafe1c6386762d62df4c18b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-18 15:22:47 -0300

    Saved libby/main.py @2024-09-18 18:22

--- libby/main.py
+++ libby/main.py
@@ -5,7 +5,7 @@ casa = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/
 
 def HistoriaPrimeiraParte ():
 	primeiraCena = Cena(img = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-aed7eee9-6be0-49e4-95e6-96524e947e230251675624545434667400.jpg&dcHint=BrazilSouth&fileToken=e02f9fea-6d7e-41e9-a241-8400357fab0b")
-	pollyPlummer= Elemento(img=blob:null/dc39cb2c-ce84-45a4-af14-fb90e96b01d1, 
+	pollyPlummer= Elemento(img=https://i.pinimg.com/736x/24/01/8f/24018f5afb82cc0099a9396a668579ac.jpg, 
                             tit="pollyplummer"
                             style=dict(left=150, top=60, width=60, height=200))
 
