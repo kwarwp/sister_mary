af323a96b7f6322b05c8e6fc797f12c4a3f9f63b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-18 15:17:57 -0300

    Saved libby/__log__.py @2024-09-18 18:17

--- libby/__log__.py
+++ libby/__log__.py
@@ -66,3 +66,10 @@ SyntaxError: invalid syntax
                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Sep 18 2024 15:17:57.418 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  pollyPlummer= Elemento(img=https://i.pinimg.com/736x/24/01/8f/24018f5afb82cc0099a9396a668579ac.jpg, 
+                                   ^
+SyntaxError: invalid syntax
+'''},
