9608f80a01f0e97e10907110fcb0267233ec333d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-18 15:17:02 -0300

    Saved libby/main.py @2024-09-18 18:17

--- libby/main.py
+++ libby/main.py
@@ -1,9 +1,15 @@
 # sister_mary.libby.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
-casa = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-eb73e2e7-2450-4c5a-b58a-feebc55fa2ef0251675625239095498400.jpg"
+casa = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-aed7eee9-6be0-49e4-95e6-96524e947e230251675624545434667400.jpg&dcHint=BrazilSouth&fileToken=e02f9fea-6d7e-41e9-a241-8400357fab0b"
 
-def HistoriaPrimeiraParte
-	primeiraCena = Cena(img"https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-aed7eee9-6be0-49e4-95e6-96524e947e230251675624545434667400.jpg")
+def HistoriaPrimeiraParte ():
+	primeiraCena = Cena(img = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-aed7eee9-6be0-49e4-95e6-96524e947e230251675624545434667400.jpg&dcHint=BrazilSouth&fileToken=e02f9fea-6d7e-41e9-a241-8400357fab0b")
+	pollyPlummer= Elemento(img=blob:null/dc39cb2c-ce84-45a4-af14-fb90e96b01d1, 
+                            tit="pollyplummer"
+                            style=dict(left=150, top=60, width=60, height=200))
+
+
 	primeiraCena.vai()
-HistoriaPrimeiraParte()
+
+HistoriaPrimeiraParte() 
