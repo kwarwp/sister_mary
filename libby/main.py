d34cad61a8b8274db6d08df0bde35a405f02b49b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-18 14:16:12 -0300

    Saved libby/main.py @2024-09-18 17:16

--- libby/main.py
+++ libby/main.py
@@ -1,9 +1,9 @@
 # sister_mary.libby.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
-explorandoPorao = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-eb73e2e7-2450-4c5a-b58a-feebc55fa2ef0251675625239095498400.jpg&dcHint=BrazilSouth&fileToken=e02f9fea-6d7e-41e9-a241-8400357fab0b"
+casa = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-eb73e2e7-2450-4c5a-b58a-feebc55fa2ef0251675625239095498400.jpg"
 
-def Historia1 ():
-	primeiraCena = Cena(img = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-eb73e2e7-2450-4c5a-b58a-feebc55fa2ef0251675625239095498400.jpg&dcHint=BrazilSouth&fileToken=e02f9fea-6d7e-41e9-a241-8400357fab0b")
+def HistoriaPrimeiraParte
+	primeiraCena = Cena(img"https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-aed7eee9-6be0-49e4-95e6-96524e947e230251675624545434667400.jpg")
 	primeiraCena.vai()
-Historia1()
+HistoriaPrimeiraParte()
