4ef35123d4dbbc459a86c5c6d46534e1ede60fb0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-18 13:11:53 -0300

    Saved libby/main.py @2024-09-18 16:11

--- libby/main.py
+++ libby/main.py
@@ -1,9 +1,9 @@
 # sister_mary.libby.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
-explorandoPorao = "view-source:https://designer.microsoft.com/image-creator"
+explorandoPorao = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-eb73e2e7-2450-4c5a-b58a-feebc55fa2ef0251675625239095498400.jpg&dcHint=BrazilSouth&fileToken=e02f9fea-6d7e-41e9-a241-8400357fab0b"
 
 def Historia1 ():
-	primeiraCena = Cena(img = "view-source:https://designer.microsoft.com/image-creator")
-	primeiraCnea.vai()
+	primeiraCena = Cena(img = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/c1c69227-99e7-43ad-8f75-665772c6d056/DallEGeneratedImages/dalle-eb73e2e7-2450-4c5a-b58a-feebc55fa2ef0251675625239095498400.jpg&dcHint=BrazilSouth&fileToken=e02f9fea-6d7e-41e9-a241-8400357fab0b")
+	primeiraCena.vai()
 Historia1()
