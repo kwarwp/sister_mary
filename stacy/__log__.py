0ed5c664466d7b30112ec2df23bf78e2d2572d01
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-18 12:41:50 -0300

    Saved stacy/__log__.py @2024-09-18 15:41

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -173,3 +173,23 @@ TypeError: 'NoneType' object is not callable
     self.reg = int(input("escolha um número:"))
 ValueError: invalid literal for int() with base 10: 'ok'
 '''},
+{'date': 'Wed Sep 18 2024 12:41:49.935 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 62
+    Calculadora().run()
+  module <module> line 38
+    while self.operate():
+  module <module> line 27
+    self.reg = int(input("escolha um número:"))
+ValueError: invalid literal for int() with base 10: ''
+'''},
