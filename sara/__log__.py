17517ae62e4f484db328f618eaef08949f3685ce
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-20 11:50:31 -0300

    Created sara/__log__.py @2024-09-20 14:50

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Fri Sep 20 2024 11:50:29.635 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  print(choice(range(100))
+       ^
+SyntaxError: Unbalanced bracket (
+'''},
