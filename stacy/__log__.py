782a9e79ea8026729556ed09d6f0c51050dc817b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-20 11:44:20 -0300

    Saved stacy/__log__.py @2024-09-20 14:44

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -193,3 +193,23 @@ ValueError: invalid literal for int() with base 10: 'ok'
     self.reg = int(input("escolha um número:"))
 ValueError: invalid literal for int() with base 10: ''
 '''},
+{'date': 'Fri Sep 20 2024 11:44:19.726 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 62
+    Calculadora().run()
+  module <module> line 38
+    while self.operate():
+  module <module> line 27
+    self.reg = int(input("escolha um número:"))
+ValueError: invalid literal for int() with base 10: 'q'
+'''},
