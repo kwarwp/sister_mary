1511e16212904a00960a182a35931fca80ea33ac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-20 13:11:12 -0300

    Saved sara/main.py @2024-09-20 16:11

--- sara/main.py
+++ sara/main.py
@@ -33,8 +33,8 @@ class Pista:
         y0, y1 = self.inicio[1] - self.largura,self.inicio[1] + self.largura
         y2, y3 = self.fim[1] - self.largura,self.fim[1] + self.pista[1] + self.largura 
         x0, x1, x2, x3, y0, y1, y2, y3 = -1, 4, 2, 4, 3, 5, -1, 5 
-        oka = (x0 <= x <= x1) and (y1 <= y <= y0)
-        okb = (x2 <= x <= x3) and (y3 <= y <= y2)
+        oka = (x0 <= x <= x1) and (y0 <= y <= y1)
+        okb = (x2 <= x <= x3) and (y2 <= y <= y3)
         ok = oka or okb
         return ok
         
@@ -58,8 +58,8 @@ if __name__ == "__main__":
     carro.move(*pista.inicio)
     print(carro.posicao, pista.dentro(*carro.posicao))
     [carro.anda() for _ in range(3)]
-    print(carro.posicao, carro.tempo)
+    print(carro.posicao, carro.tempo, pista.dentro(*carro.posicao))
     carro.velocidade = [0, -1]
     [carro.anda() for _ in range(4)]
-    print(carro.posicao, carro.tempo)
+    print(carro.posicao, carro.tempo, pista.dentro(*carro.posicao))
         
