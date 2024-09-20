06f58d3b11b25a993acc7ed3eaeb10eb42f7dd3a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-20 13:23:59 -0300

    Saved sara/__log__.py @2024-09-20 16:23

--- sara/__log__.py
+++ sara/__log__.py
@@ -36,3 +36,24 @@ SyntaxError: invalid syntax
                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Sep 20 2024 13:23:59.221 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''[0, 4] True
+(3, 4) 3 True
+(3, 0) 7 True
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 75
+    carro.manual()
+  module <module> line 71
+    print("encerrou", pista.encerrou(carro.posicao))
+TypeError: encerrou() missing 1 positional argument: y
+'''},
