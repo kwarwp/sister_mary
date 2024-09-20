04a70143a8d1b48133497a2ae13a5de8f72bf3ac
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-20 13:25:36 -0300

    Saved sara/__log__.py @2024-09-20 16:25

--- sara/__log__.py
+++ sara/__log__.py
@@ -80,3 +80,26 @@ Traceback (most recent call last):
     return (x0 <= x <= x1) and (y2 <= y <= y3)
 NameError: name 'x0' is not defined
 '''},
+{'date': 'Fri Sep 20 2024 13:25:35.590 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''[0, 4] True
+(3, 4) 3 True
+(3, 0) 7 True
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 76
+    carro.manual()
+  module <module> line 72
+    print("encerrou", pista.encerrou(*carro.posicao))
+  module <module> line 32
+    return (x0 <= x <= x3) and (y2 <= y <= y3)
+NameError: name 'x0' is not defined
+'''},
