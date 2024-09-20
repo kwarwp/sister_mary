dc933c976d0f7caa39c1d6ed175c00227d992417
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-20 11:53:07 -0300

    Created sara/main.py @2024-09-20 14:53

---
+++ sara/main.py
@@ -0,0 +1,24 @@
+# sister_mary.sara.main.py
+""""Experimentando IA.
+
+.. codeauthor:: Carlo Oliveira <carlo@nce.ufrj.br>
+.. codeauthor:: Miguel Roman <migdukaroma@gmail.com>
+
+Changelog
+---------
+.. versionadded::    24.09
+   |br| Início (20).
+
+|   **Open Source Notification:** This file is part of open source program **Carro IA**
+|   **Copyright © 2024  Carlo Oliveira** <carlo@nce.ufrj.br>,
+|   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <http://is.gd/3Udt>`_.
+|   `Labase <http://labase.selfip.org/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
+"""
+import random
+from random import choice
+print(choice(range(100)))
+
+
+class Pista:
+    def __init__(self):
+        self.pista = [3,4]
