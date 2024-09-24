c8ff4986dee90fe69a0a08525e03c802352de2fe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 15:31:29 -0300

    Saved libby/main.py @2024-09-24 18:31

--- libby/main.py
+++ libby/main.py
@@ -1,4 +1,4 @@
 # sister_mary.libby.main.py_Raquel Brayner
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
-LinkCidadeCharn = "file:///C:/Users/brayn/Downloads/Designer.jpeg"
+LinkCidadeCharn = "https://img.elo7.com.br/product/original/1AD3471/painel-1x0-70-salao-de-festa-salao-de-festa.jpg"
