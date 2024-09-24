109526199352b52beb2db110740bc2b7597cc4e7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 15:29:55 -0300

    Saved libby/main.py @2024-09-24 18:29

--- libby/main.py
+++ libby/main.py
@@ -1,4 +1,4 @@
 # sister_mary.libby.main.py_Raquel Brayner
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
-LinkCidadeCharn ="https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg"
+LinkCidadeCharn = "C:\Users\brayn\Downloads\Designer.jpeg"
