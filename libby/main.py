fa3fe39ebce25cb6f4dc479cbd58811c6baef564
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 15:45:24 -0300

    Saved libby/main.py @2024-09-24 18:45

--- libby/main.py
+++ libby/main.py
@@ -1,4 +1,11 @@
 # sister_mary.libby.main.py_Raquel Brayner
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
+# sanvando o link no código primeiro
 LinkCidadeCharn = "https://img.elo7.com.br/product/original/1AD3471/painel-1x0-70-salao-de-festa-salao-de-festa.jpg"
+
+#instruções para a primeira parte da históra
+def hitoriaIntroducao():
+	cenaChan = cena (img = "")
+	cenaChan.vai()    
+historiaIntruducao()
