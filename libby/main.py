b8d2344cd67feb5e93d8f016e74f21873ffc3181
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 16:45:32 -0300

    Saved libby/main.py @2024-09-24 19:45

--- libby/main.py
+++ libby/main.py
@@ -4,8 +4,7 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 # salvando o link no código primeiro
 LinkCidadeCharn = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62"
 
-STYLE['width'] = 1000
-STYLE['height'] = "800px"
+
 
 #instruções para a primeira parte da históra
 def Historia():
