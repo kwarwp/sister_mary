780808f7bc9acb392fe01cfc814a073817d3feb6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 15:53:30 -0300

    Saved libby/main.py @2024-09-24 18:53

--- libby/main.py
+++ libby/main.py
@@ -2,11 +2,10 @@
 from _spy.vitollino.main import Cena, Elemento, Texto 
 
 # salvando o link no código primeiro
-LinkCidadeCharn = "file:///C:/Users/brayn/Downloads/Designer.jpeg"
-
+LinkCidadeCharn = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62"
 #instruções para a primeira parte da históra
 def Historia():
-	cenaCharn = Cena(img = "file:///C:/Users/brayn/Downloads/Designer.jpeg")
+	cenaCharn = Cena(img = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62")
 	cenaCharn.vai()    
     
 Historia()
