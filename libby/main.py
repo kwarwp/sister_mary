211cccbb3177d673ab85b2a2f12fbf6b8e526d69
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 16:50:04 -0300

    Saved libby/main.py @2024-09-24 19:50

--- libby/main.py
+++ libby/main.py
@@ -1,8 +1,8 @@
 # sister_mary.libby.main.py_Raquel Brayner
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 
-STYLE['width'] = 1000
-STYLE['height'] = "500px"
+STYLE['width'] = 900
+STYLE['height'] = "600px"
 
 # salvando o link no código primeiro
 LinkCidadeCharn = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62"
