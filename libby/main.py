80e84c0076c2f78776b79d77c15300c59c7814e4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 19:19:09 -0300

    Saved libby/main.py @2024-09-24 22:19

--- libby/main.py
+++ libby/main.py
@@ -10,7 +10,7 @@ LinkPortas = "https://designerapp.officeapps.live.com/designerapp/document.ashx?
 
 #instruções para a primeira parte da históra
 def HistoriaIntroducao():
-	cenaCharn = Cena(img = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62")
+	cenaCharn = Cena(img = LinkCidadeCharn)
 	textoCharn = Texto(cenaCharn, "No universo de fantasia das Crónicas Nárnia , Polly Plummer, uma aventureira e curiosa garotinha de 11 anos, tenta encontrar a chave correta para abrir a porta que a transportará de volta para o jardim da Nárnia, um lugar construído e protegido pelo leão Aslam.")
 	textoCharn.vai()
 	cenaCharn.vai() 
