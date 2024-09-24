bcbf70138c9c29643e70bb8f624a46fe6ff6c9cf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 15:48:03 -0300

    Saved libby/__log__.py @2024-09-24 18:48

--- libby/__log__.py
+++ libby/__log__.py
@@ -173,3 +173,21 @@ NameError: name 'cena' is not defined
     	cenaChan = cena (img = "https://img.elo7.com.br/product/original/1AD3471/painel-1x0-70-salao-de-festa-salao-de-festa.jpg")
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Tue Sep 24 2024 15:48:02.573 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 12
+    historiaIntroducao()
+  module <module> line 9
+    	cenaChan = cena(img = "https://img.elo7.com.br/product/original/1AD3471/painel-1x0-70-salao-de-festa-salao-de-festa.jpg")
+NameError: name 'cena' is not defined
+'''},
