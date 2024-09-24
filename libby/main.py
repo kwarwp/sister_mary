956363ffd0b6c3ffcbd72ceeefcd85305b750273
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 16:03:04 -0300

    Saved libby/main.py @2024-09-24 19:03

--- libby/main.py
+++ libby/main.py
@@ -3,9 +3,11 @@ from _spy.vitollino.main import Cena, Elemento, Texto
 
 # salvando o link no código primeiro
 LinkCidadeCharn = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62"
+
 #instruções para a primeira parte da históra
 def Historia():
 	cenaCharn = Cena(img = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62")
+	texIntruducao = Texto(cenaCharn, "No universo de fantasia das Crónicas Nárnia , Polly Plummer, uma aventureira e curiosa garotinha de 11 anos, tenta encontrar a chave correta para abrir a porta que a transportará de volta para o jardim da Nárnia, um lugar construído e protegido pelo leão Aslam.")
 	cenaCharn.vai()    
     
 Historia()
