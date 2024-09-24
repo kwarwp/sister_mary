94fa9dd644325491099fcd094146202d6c849ce0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 16:17:09 -0300

    Saved libby/main.py @2024-09-24 19:17

--- libby/main.py
+++ libby/main.py
@@ -9,8 +9,8 @@ Pergaminho = "https://designerapp.officeapps.live.com/designerapp/document.ashx?
 #instruções para a primeira parte da históra
 def Historia():
 	cenaCharn = Cena(img = "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-221735ad-13e5-45a5-ad43-806d169277440251675098881199351800.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62")
-	Pergaminho = Elemento (img = "Pergaminho", 
-                            style=dict(left=150, width=60, height=200))
+	Pergaminho = Elemento(img="Pergaminho", 
+                           style=dict(left=150, width=60, height=200))
 	Pergaminho.entra(cenaCharn)    
 	cenaCharn.vai()    
     
