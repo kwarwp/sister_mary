62ec9801df5d147566f9b779c8a8890b4ecbe54f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 20:00:17 -0300

    Saved libby/main.py @2024-09-24 23:00

--- libby/main.py
+++ libby/main.py
@@ -12,17 +12,19 @@ LinkPortas = "https://designerapp.officeapps.live.com/designerapp/document.ashx?
 def HistoriaIntroducao():
 	cenaCharn = Cena(img = LinkCidadeCharn)
 	textoCharn = Texto(cenaCharn, "No universo de fantasia das Crónicas Nárnia , Polly Plummer, uma aventureira e curiosa garotinha de 11 anos, fica presa na devastada cidade de Charn.Ajude-a a encontrar a chave correta para abrir a porta que a transportará de volta para o jardim da Nárnia, um lugar mágico construído e protegido pelo leão Aslam")
-	textoCharn.vai = lambda: HistoriaPortas()
-	cenaCharn.vai() 
 	textoCharn.vai()
-    
+	cenaCharn.vai() 
+	HistoriaPortas.vai()
+HistoriaIntroducao()
+
+
 def HistoriaPortas():
 	cenaPortas = Cena(img= "https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/bf4d9c8d-f5cf-4571-9ac3-b26154c673f6/DallEGeneratedImages/dalle-7ccf45fd-d91c-45e0-a5a9-6fd1c7eefcf10251675091894855557900.jpg&dcHint=BrazilSouth&fileToken=1b21d0be-54e6-4bac-8f89-f042cff4cd62")
 	textoPortas = Texto(cenaPortas, "Ajude Polly encontrar a chave correta para sair da cidade devastada Chan e escapar da feiticeira. ")
 	textoPortas.vai()
 	cenaPortas.vai()
 
-HistoriaIntroducao()
+HistoriaPortas()
 
 
 
