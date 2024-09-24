c31064a2649f2a28155e26b9bf9d157c05959de3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-24 17:30:25 -0300

    Saved libby/__log__.py @2024-09-24 20:30

--- libby/__log__.py
+++ libby/__log__.py
@@ -471,3 +471,21 @@ AttributeError: 'function' object has no attribute 'vai'
     	HistoriaPortas.vai()
 AttributeError: 'function' object has no attribute 'vai'
 '''},
+{'date': 'Tue Sep 24 2024 17:30:24.630 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    HistoriaIntroducao()
+  module <module> line 17
+    	HistoriaPortas.vai()
+AttributeError: 'function' object has no attribute 'vai'
+'''},
