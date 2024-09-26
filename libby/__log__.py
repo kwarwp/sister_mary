680e1f223c98d6dac660fd74eb6ecacc2f10c958
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-26 18:15:46 -0300

    Saved libby/__log__.py @2024-09-26 21:15

--- libby/__log__.py
+++ libby/__log__.py
@@ -1073,3 +1073,9 @@ Exception: obj is undefined
   module '$exec_320' line 18
 HistoriaIntroducao()
 '''},
+{'date': 'Thu Sep 26 2024 18:15:46.102 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+Exception: obj is undefined
+  module '$exec_1897' line 18
+HistoriaIntroducao()
+'''},
