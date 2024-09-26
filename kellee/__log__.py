9808e0f4c561524f3dd42410b62fd9ec481ce79a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-26 15:23:31 -0300

    Saved kellee/__log__.py @2024-09-26 18:23

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -62,3 +62,23 @@ AttributeError: 'Cena' object has no attribute 'norte'
     cidadeCharn.norte.vai()
 NameError: name 'cidadeCharn' is not defined
 '''},
+{'date': 'Thu Sep 26 2024 15:23:31.212 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    historia=Sala(n=cidadeCharn_norte, s=IMAGEM_SUL, L=cidadeCharn_leste, O=portas_oeste)
+  module _spy.vitollino.main line 1015
+    Sala.c(**kwargs)
+  module _spy.vitollino.main line 1043
+    setattr(Sala, nome, Sala(nome=nome, **cena))
+TypeError: 'Cena' object is not iterable
+'''},
