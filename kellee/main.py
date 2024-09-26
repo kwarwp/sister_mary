d8b8e29dffc282ccdbeeb0c9b92b64c242f0bca0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-26 15:17:31 -0300

    Created kellee/main.py @2024-09-26 18:17

---
+++ kellee/main.py
@@ -0,0 +1,35 @@
+# sister_mary.kellee.main.py
+from _spy.vitollino.main import Cena,Sala, ELement, Texto, STYLE
+
+STYLE['width'] = 900
+STYLE['height'] = "600px"
+
+IMAGEM_NORTE = "https://imgur.com/inUG43J" #CIDADECHARN
+IMAGEM_SUL = "https://imgur.com/GnOVugz" #PORTAS
+#IMAGEM_LESTE =
+#IMAGEM_OESTE =
+
+cidadeCharn_norte = Cena(IMAGEM_NORTE)
+portas_sul= Cena(IMAGEM_SUL)
+
+historia=Sala(n=cidadeCharn_norte, s=IMAGEM_SUL)
+
+cidadeCharn_norte.norte.vai()
+portas_sul.sul.vai()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
