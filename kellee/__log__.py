832d6900b0fcd05e288685845d70df653d3910f7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-26 15:25:28 -0300

    Saved kellee/__log__.py @2024-09-26 18:25

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -109,3 +109,23 @@ TypeError: 'Cena' object is not iterable
                                              ^
 SyntaxError: trailing comma not allowed without surrounding parentheses
 '''},
+{'date': 'Thu Sep 26 2024 15:25:27.712 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 15
+    historia=Sala(n=cidadeCharn_norte, s=portas_sul, L=cidadeCharn_leste, O=portas_oeste)
+  module _spy.vitollino.main line 1015
+    Sala.c(**kwargs)
+  module _spy.vitollino.main line 1043
+    setattr(Sala, nome, Sala(nome=nome, **cena))
+TypeError: 'Cena' object is not iterable
+'''},
