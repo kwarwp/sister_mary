5972ae6fbd18ff89ade4e13c98ba053c6cc362e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-26 15:23:29 -0300

    Saved kellee/main.py @2024-09-26 18:23

--- kellee/main.py
+++ kellee/main.py
@@ -6,16 +6,21 @@ STYLE['height'] = "600px"
 
 IMAGEM_NORTE = "https://imgur.com/inUG43J" #CIDADECHARN
 IMAGEM_SUL = "https://imgur.com/GnOVugz" #PORTAS
-#IMAGEM_LESTE =
-#IMAGEM_OESTE =
+IMAGEM_LESTE = "https://imgur.com/inUG43J"
+IMAGEM_OESTE ="https://imgur.com/GnOVugz"
 
 cidadeCharn_norte = Cena(IMAGEM_NORTE)
 portas_sul= Cena(IMAGEM_SUL)
+cidadeCharn_leste = Cena(IMAGEM_LESTE)
+portas_oeste= Cena(IMAGEM_OESTE)
 
-historia=Sala(n=cidadeCharn_norte, s=IMAGEM_SUL)
+
+historia=Sala(n=cidadeCharn_norte, s=IMAGEM_SUL, L=cidadeCharn_leste, O=portas_oeste)
 
 cidadeCharn_norte.vai()
 portas_sul.vai()
+cidadeCharn_leste.vai()
+portas_oeste.vai()
 
 
 
