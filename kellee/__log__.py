8b6b08b09f293fd2430c2265c9d36c6fda8f688d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-26 18:14:30 -0300

    Saved kellee/__log__.py @2024-09-26 21:14

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -270,3 +270,34 @@ Traceback (most recent call last):
   module <module> line 2
     from _spy.vitollino.main import Cena,Sala,Texto,ELemento, STYLE
 '''},
+{'date': 'Thu Sep 26 2024 18:14:29.986 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from _spy.vitollino.main import Cena,Sala,Texto, ELemento, STYLE
+ImportError: cannot import name 'ELemento'
+
+ImportError
+Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 268
+    action()
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 2
+    from _spy.vitollino.main import Cena,Sala,Texto, ELemento, STYLE
+'''},
