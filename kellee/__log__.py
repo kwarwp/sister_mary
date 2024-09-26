1ea4ac153ec90d9511125d5ff501985820fa56cd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-09-26 15:49:25 -0300

    Saved kellee/__log__.py @2024-09-26 18:49

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -181,3 +181,19 @@ NameError: name 'Textointroducao' is not defined
     Textointroducao(cidadeCharn_norte, """No universo de fantasia das Cróicas Nárnia , Polly Plummer, uma aventureira e curiosa garotinha de 11 anos, fica presa na devastada cidade de Charn.
 NameError: name 'Textointroducao' is not defined
 '''},
+{'date': 'Thu Sep 26 2024 15:49:24.649 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    TextoPortas=(portas_sul, """Fala polly: Rápido! Só temos 3 tentativas!""").vai()
+AttributeError: 'tuple' object has no attribute 'vai'
+'''},
