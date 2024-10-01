d2bb37cb043a9c639e66a52e9b6c1df05ef81590
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-10-01 18:30:30 -0300

    Saved kellee/__log__.py @2024-10-01 21:30

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -555,3 +555,19 @@ NameError: name 'SegundaCena' is not defined
     historia=Sala(n=cidadeCharn_norte, l=SegundaCena_leste, s=terceiraCena_sul, o=quartaCena_oeste)
 NameError: name 'SegundaCena_leste' is not defined
 '''},
+{'date': 'Tue Oct 01 2024 18:30:29.788 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 23
+    chaves=ELemento(ElementoChave,h=250,w=200, x=100, y=300)
+NameError: name 'ELemento' is not defined
+'''},
