a9d88be8e4908b3b722bb6b80fef51d5b5004dd7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-10-01 17:54:41 -0300

    Saved kellee/main.py @2024-10-01 20:54

--- kellee/main.py
+++ kellee/main.py
@@ -7,27 +7,27 @@ STYLE['height'] = "600px"
 ELementoPolly="https://i.imgur.com/ON2IIrs.png"
 
 
-IMAGEM_NORTE = "https://i.imgur.com/inUG43J.png" #CIDADECHARN
-IMAGEM_LESTE ="https://i.imgur.com/BZq67xa.png"
-IMAGEM_SUL = "https://i.imgur.com/BZq67xa.png" #chamada para abrir as portas 
-IMAGEM_OESTE ="https://i.imgur.com/rwi0tql.png" #cadeira
+IMAGEM_NORTE = "https://i.imgur.com/inUG43J.png" #CIDADECHARN/introduçã
+IMAGEM_LESTE ="https://i.imgur.com/BZq67xa.png"#chamada para abrir as portas
+IMAGEM_SUL = "https://i.imgur.com/FkeZ1FX.png" #portas 
+IMAGEM_OESTE ="https://i.imgur.com/rwi0tql.png" #cadeira teste
 
 
 cidadeCharn_norte = Cena(IMAGEM_NORTE)
-chamada_sul= Cena(IMAGEM_SUL)
-cidadeCharn_leste = Cena(IMAGEM_LESTE)
-portas_oeste= Cena(IMAGEM_OESTE)
-#Polly = Elemento(ELementoPolly,style=dict(height=30,width=150, left=600, top=20))
+segundaCena_leste = Cena(IMAGEM_LESTE)
+terceiraCena_sul= Cena(IMAGEM_SUL)
+quartaCena_oeste= Cena(IMAGEM_OESTE)
+
 Polly = Elemento(ELementoPolly,h=250,w=200, x=100, y=300)
 
 def segundaParte():
-	Polly.entra(chamada_sul)
-	TextoPortas=Texto(chamada_sul, """Rápido! Só temos 3 tentativas!""")
+	Polly.entra(segundaCena_leste)
+	TextoPortas=Texto(segundaCena_leste, """Rápido! Só temos 3 tentativas!""")
 	TextoPortas.vai()
-	chamada_sul.vai()
+	segundaCena_leste.vai()
 	
 
-historia=Sala(n=cidadeCharn_norte, l=chamada_sul, s=cidadeCharn_leste, o=portas_oeste)
+historia=Sala(n=cidadeCharn_norte, l=segundaCena_leste, s=terceiraCena_sul, o=quartaCena_oeste)
 TextoIntroducao=Texto(cidadeCharn_norte,"""No universo de fantasia das Crónicas Nárnia""", foi=segundaParte).vai()
 
 
