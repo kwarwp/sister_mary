cdc28cbb4c2424232039d0609a3f3d86ee86c4c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-10-01 18:42:52 -0300

    Saved kellee/main.py @2024-10-01 21:42

--- kellee/main.py
+++ kellee/main.py
@@ -5,6 +5,7 @@ STYLE['width'] = 900
 STYLE['height'] = "600px"
 
 ELementoPolly="https://i.imgur.com/ON2IIrs.png"
+ElementoChave="https://i.imgur.com/5LAft4z.png"
 
 
 IMAGEM_NORTE = "https://i.imgur.com/inUG43J.png" #CIDADECHARN/introduçã
@@ -19,13 +20,21 @@ terceiraCena_sul= Cena(IMAGEM_SUL)
 quartaCena_oeste= Cena(IMAGEM_OESTE)
 
 Polly = Elemento(ELementoPolly,h=250,w=200, x=100, y=300)
+chaves= Elemento(ElementoChave,h=250,w=200, x=100, y=300)
+chaves.elt.style={'background-size':"400% 100%"}
 
-def segundaParte():
+
+def terceiraParte(*_):
+	terceiraCena_sul.vai()
+	chaves.entra(terceiraCena_sul)
+
+
+def segundaParte(*_):
 	Polly.entra(segundaCena_leste)
-	TextoPortas=Texto(segundaCena_leste, """Rápido! Só temos 3 tentativas!""")
+	TextoPortas=Texto(segundaCena_leste, """Rápido! Só temos 3 tentativas!""", foi=terceiraParte)
 	TextoPortas.vai()
 	segundaCena_leste.vai()
-	
+
 
 historia=Sala(n=cidadeCharn_norte, l=segundaCena_leste, s=terceiraCena_sul, o=quartaCena_oeste)
 TextoIntroducao=Texto(cidadeCharn_norte,"""No universo de fantasia das Crónicas Nárnia""", foi=segundaParte).vai()
