8659b6e5bcdac7d823d33e8a1d71d8e4a6d1a1fb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-10-01 15:04:55 -0300

    Saved kellee/__log__.py @2024-10-01 18:04

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -340,3 +340,19 @@ NameError: name 'IMAGEM_LESTE' is not defined
     portas_oeste= Cena(IMAGEM_OESTE)
 NameError: name 'IMAGEM_OESTE' is not defined
 '''},
+{'date': 'Tue Oct 01 2024 15:04:55.27 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 27
+    historia=Sala(n=cidadeCharn_norte, s=portas_sul, l=cidadeCharn_leste, o=portas_oeste)
+NameError: name 'cidadeCharn_leste' is not defined
+'''},
