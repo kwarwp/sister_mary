dd2c75227ad945f485134d502ce44c2e3e5f5864
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-10-01 17:44:31 -0300

    Saved kellee/__log__.py @2024-10-01 20:44

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -491,3 +491,19 @@ SyntaxError: invalid syntax
     historia=Sala(n=cidadeCharn_norte, l=chamada_sul, s=cidadeCharn_leste, o=portas_oeste)
 NameError: name 'cidadeCharn_leste' is not defined
 '''},
+{'date': 'Tue Oct 01 2024 17:44:31.322 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    historia=Sala(n=cidadeCharn_norte, l=SegundaCena, s=cidadeCharn_leste, o=portas_oeste)
+NameError: name 'SegundaCena' is not defined
+'''},
