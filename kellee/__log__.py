137afbb1d29d8df6544347402454be8e1973dcf1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-10-01 16:06:36 -0300

    Saved kellee/__log__.py @2024-10-01 19:06

--- kellee/__log__.py
+++ kellee/__log__.py
@@ -391,3 +391,10 @@ SyntaxError: invalid syntax
                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Oct 01 2024 16:06:35.869 GMt-0300 (Brasilia Standard Time) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  Polly=ELemento(Polly, style=dict(height=60,widht=60, left=600, top=20)Cena = chamada_sul)
+                                                                         ^
+SyntaxError: invalid syntax
+'''},
