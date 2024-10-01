96a9d1604ad6701bf861e0ea0591984b1919efe4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2024-10-01 19:28:59 -0300

    Saved kellee/main.py @2024-10-01 22:28

--- kellee/main.py
+++ kellee/main.py
@@ -26,7 +26,7 @@ Polly = Elemento(ELementoPolly,h=300,w=350, x=50, y=300)
 
 chaves1= Elemento(ElementoChave1,h=100,w=200, x=50, y=500)
 chaves2= Elemento(ElementoChave2,h=100,w=200, x=250, y=500)
-chaves3= Elemento(ElementoChave3,h=250,w=200, x=600, y=400)
+chaves3= Elemento(ElementoChave3,h=100,w=200, x=600, y=400)
 chaves4= Elemento(ElementoChave4,h=250,w=200, x=700, y=100)
 #chaves.elt.style={'background-size':"400% 100%"} ----teste hacking
 
