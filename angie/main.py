a0621e4265ead36c9741ef16e019aaa60d5cfad2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-13 18:40:55 -0300

    Created angie/main.py @2025-02-13 21:40

---
+++ angie/main.py
@@ -0,0 +1,12 @@
+# sister_mary.angie.main.py
+# Projeto Jogo Miguel 13/02/2025
+from _spy.vitollino.main import Cena, Elemento
+from browser import document
+CENA0 = "https://i.imgur.com/kJrBXt8.jpeg"
+CENA1 = "https://imgur.com/0cqfufR.jpeg"
+HEROI0 = "https://i.imgur.com/WQ2l5kM.png"
+
+aqui = Cena(CENA0)
+aqui.vai()
+coisa = Elemento(HEROI0)
+coisa.entra(aqui)
