19826b552059a0f1fe983db73f9728476a271d35
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-13 19:09:11 -0300

    Created angie/__log__.py @2025-02-13 22:09

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Feb 13 2025 19:09:10.163 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  match evento.keyCode:
+         ^
+SyntaxError: invalid syntax
+'''},
