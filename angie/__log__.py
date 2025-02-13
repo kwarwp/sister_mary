bc8b8198dc96dda866dcf5566a84110d61f3838c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-13 19:19:39 -0300

    Saved angie/__log__.py @2025-02-13 22:19

--- angie/__log__.py
+++ angie/__log__.py
@@ -6,3 +6,10 @@
          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Feb 13 2025 19:19:38.936 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  indo = dict(d37=(-10,0), d38=(0,-10), d39=(10, 0), d40=(0, 10)
+                                                                                                      ^
+SyntaxError: invalid syntax
+'''},
