bd92b97f73fab153cea7a13acca32e3c231d39e3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-18 16:37:55 -0300

    Saved parisa/__log__.py @2025-02-18 19:37

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -106,3 +106,23 @@ NameError: name 'randint' is not defined
     tr = [self.sprite(randint(0,100)*100, randint(50,100)*layer+200, randint(0,20), layer)
 NameError: name 'randint' is not defined
 '''},
+{'date': 'Tue Feb 18 2025 16:37:54.937 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    Lax()
+  module <module> line 14
+    self.scenery()
+  module <module> line 21
+    tr = [self.sprite(randint(0,100)*100, randint(50,100)*layer+200, randint(0,20), layer)
+NameError: name 'randint' is not defined
+'''},
