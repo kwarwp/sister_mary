6da4eb76580ddd4bcefea26c0ade39d0071d64fe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-18 15:36:07 -0300

    Created parisa/__log__.py @2025-02-18 18:36

---
+++ parisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Feb 18 2025 15:36:06.270 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 18
+  return Elemento(FLORA, w=300 h=300, x=x, y=y, tipo="20% 25%", cena=self.cena)
+                                ^
+SyntaxError: invalid syntax
+'''},
