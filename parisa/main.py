750a0c54e25aab53ce00dcfd96d361e9542f2a8e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-18 17:04:36 -0300

    Saved parisa/main.py @2025-02-18 20:04

--- parisa/main.py
+++ parisa/main.py
@@ -5,11 +5,12 @@ STYLE.update(width=1250, height="650px")
 FLORA = "https://i.imgur.com/n3GnL9B.png"
 MATA0 = "https://i.imgur.com/TT2FKyu.jpeg"
 FX, FY = 5, 4
-TREES = 600
+TREES = 500
 
 class Lax:
     def __init__(self):
         self.c = Cena(MATA0)
+        c.elt.style.overflow="hidden"
         # self.e = Elemento(FLORA,w=1250,h=1000, cena = c)
         self.c.vai()
         self.scenery()
@@ -18,15 +19,16 @@ class Lax:
         t = self.sprite(300, 0, 6, 4)
         t = self.sprite(600, 0, 12, 8)
         
-    def scenery(self, trees=2):
-        tr = [self.sprite(randint(0,100)*100, randint(50,100)*layer+200, randint(0,20), layer)
-        for layer in range(1,5) for _ in range(0, trees)]
+    def scenery(self, trees=8):
+        tr = [self.sprite(randint(0,10)*90 -20, 300-randint(0,5)*5*layer, randint(0,20), layer)
+        for layer in range(8,1,-1) for _ in range(0, trees)]
         
     def sprite(self, x, y, item, layer):
         dw, dh, size = 100 // (FX-1), 100 // (FY-1), FX*FY
         ox, oy = item // FX, item % FX
         size = TREES - layer * 30
-        e = Elemento(FLORA, w=size, h=size, x=x, y=y+layer*15, cena=self.c)
+        e = Elemento(FLORA, w=size, h=size, x=x, y=y-layer*15, cena=self.c)
+        e.o = layer / 10 + 0.2
         e.elt.style.backgroundSize = f"{FX*100}% {FY*100}%"
         e.elt.style.backgroundPosition = f"{ox*dw}% {oy*dh}%"
         #e.elt.style.backgroundPosition = f"50% 66%"
