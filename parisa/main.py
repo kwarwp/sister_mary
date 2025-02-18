40bbc8c8c01cc8c2bf6c2cf3e24b94028f5dcc50
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-18 14:14:22 -0300

    Saved parisa/main.py @2025-02-18 17:14

--- parisa/main.py
+++ parisa/main.py
@@ -1,8 +1,20 @@
 # sister_mary.parisa.main.py
 from _spy.vitollino.main import Cena, Elemento, STYLE
-STYLE.update(width=1200, height="650px")
-FLORA = "https://i.imgur.com/ujeUjZj.png"
+STYLE.update(width=1250, height="650px")
+FLORA = "https://i.imgur.com/n3GnL9B.png"
 MATA0 = "https://i.imgur.com/TT2FKyu.jpeg"
-c = Cena(MATA0)
-e = Elemento(FLORA, cena = c)
-c.vai()
+
+class Lax:
+    def __init__(self):
+        c = Cena(MATA0)
+        self.e = Elemento(FLORA,w=1250,h=1000, cena = c)
+        c.vai()
+        
+    def sprite(self, item, layer):
+        dw, dh = 100 // 5, 100 // 4
+        ox = 
+        return Elemento(FLORA, w=f(dw%) h
+    
+        
+        
+Lax()
