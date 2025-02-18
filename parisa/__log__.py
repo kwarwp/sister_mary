023815d7e58359d4fbbeb72673e3d26b12480da7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-18 16:33:50 -0300

    Saved parisa/__log__.py @2025-02-18 19:33

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -66,3 +66,23 @@ TypeError: jitter() missing 1 positional argument: z
     tr = [self.sprite(randint(0,100)*1000, randint(50,100)*layer, randint(0,20), layer)
 NameError: name 'randint' is not defined
 '''},
+{'date': 'Tue Feb 18 2025 16:33:49.668 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 37
+    Lax()
+  module <module> line 14
+    self.scenery()
+  module <module> line 21
+    tr = [self.sprite(randint(0,100)*1000, randint(50,100)*layer, randint(0,20), layer)
+NameError: name 'randint' is not defined
+'''},
