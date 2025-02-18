f8d073f9c8e4419192bfccd2193fadbd1df60452
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-18 17:57:29 -0300

    Saved parisa/__log__.py @2025-02-18 20:57

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -171,3 +171,23 @@ SyntaxError: invalid syntax
     [lay.elt <= self.sprite(tr*(1200//trees)+randint(0,10)-100, 350-randint(0,15), randint(0,20), layer)
 AttributeError: 'Lax' object has no attribute 'layer'
 '''},
+{'date': 'Tue Feb 18 2025 17:57:29.546 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 47
+    Lax()
+  module <module> line 17
+    self.scenery()
+  module <module> line 28
+    [lay.elt <= self.sprite(tr*(1200//trees)+randint(0,10)-100, 350-randint(0,15), randint(0,20), layer)
+AttributeError: 'Lax' object has no attribute 'layers'
+'''},
