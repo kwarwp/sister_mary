2ec7fd0e729d54a2494d5d76ce331254fdbb3b41
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-18 15:36:04 -0300

    Saved parisa/main.py @2025-02-18 18:36

--- parisa/main.py
+++ parisa/main.py
@@ -3,17 +3,19 @@ from _spy.vitollino.main import Cena, Elemento, STYLE
 STYLE.update(width=1250, height="650px")
 FLORA = "https://i.imgur.com/n3GnL9B.png"
 MATA0 = "https://i.imgur.com/TT2FKyu.jpeg"
+FX, FY = 5, 4
 
 class Lax:
     def __init__(self):
-        c = Cena(MATA0)
-        self.e = Elemento(FLORA,w=1250,h=1000, cena = c)
-        c.vai()
+        self.c = Cena(MATA0)
+        # self.e = Elemento(FLORA,w=1250,h=1000, cena = c)
+        self.c.vai()
+        t = self.sprite(0, 0, 0, 0)
         
-    def sprite(self, item, layer):
-        dw, dh = 100 // 5, 100 // 4
-        ox = 
-        return Elemento(FLORA, w=f(dw%) h
+    def sprite(self, x, y, item, layer):
+        dw, dh, size = 100 // FX, 100 // FY, FX*FY
+        ox, oy = item // FX, item % FX
+        return Elemento(FLORA, w=300 h=300, x=x, y=y, tipo="20% 25%", cena=self.cena)
     
         
         
