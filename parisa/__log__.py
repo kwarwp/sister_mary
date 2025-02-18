b0462355ed7a63170e81f306e31a898df56d604d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-18 16:22:13 -0300

    Saved parisa/__log__.py @2025-02-18 19:22

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -26,3 +26,23 @@ SyntaxError: invalid syntax
     return Elemento(FLORA, w=300, h=300, x=x, y=y, tipo="20% 25%", cena=self.cena)
 AttributeError: 'Lax' object has no attribute 'cena'
 '''},
+{'date': 'Tue Feb 18 2025 16:22:12.979 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    Lax()
+  module <module> line 15
+    t = self.sprite(0, 0, 0, 1)
+  module <module> line 29
+    e = Elemento(FLORA, w=size, h=size, x=x+jitter(), y=y+layer*15+jitter(), cena=self.c)
+TypeError: jitter() missing 1 positional argument: z
+'''},
