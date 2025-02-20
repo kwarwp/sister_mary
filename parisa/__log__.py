9405569dd94520ef62db26a8641d2c0e10bddb97
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-20 15:03:35 -0300

    Saved parisa/__log__.py @2025-02-20 18:03

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -238,3 +238,25 @@ TypeError: can't add 'Elemento' object to DOMNode instance
                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Thu Feb 20 2025 15:03:35.2 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 85
+    Lax()
+  module <module> line 18
+    self.scenery()
+  module <module> line 33
+    [lay.elt <= self.sprite(150*item-300, 350, randint(0,15), 1, layer)
+  module <module> line 69
+    dw, dh = (100/conta_)*(p % FX), (100/lado_)*(p // FX)
+NameError: name 'p' is not defined
+'''},
