0300eddf846389278d9cbd3e0bb2344a150a8da1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2025-02-20 18:12:57 -0300

    Saved parisa/__log__.py @2025-02-20 21:12

--- parisa/__log__.py
+++ parisa/__log__.py
@@ -294,3 +294,23 @@ SyntaxError: invalid syntax
     dw, dh = (100/conta_)*(item % KX), (100/lado_)*(item // KX)
 ZeroDivisionError: division by zero
 '''},
+{'date': 'Thu Feb 20 2025 18:12:56.648 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 180
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 310
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 282
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 299
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 124
+    Lax()
+  module <module> line 43
+    self.scenery()        
+  module <module> line 75
+    [lay.elt <= self.sprite(150*item-3000, 350, randint(0,15), 1, layer)
+TypeError: 'int' object is not callable
+'''},
